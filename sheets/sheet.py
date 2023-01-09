d3433fb7d147857fd195be2575e4de51fd19b8ee
Jay Dong <jdong@caltech.edu>
2023-01-08 20:55:51 -0800

    Create sheet class

--- sheets/sheet.py
+++ sheets/sheet.py
@@ -1,1 +1,50 @@
-from typing import *
+class Sheet:
+    def __init__(self, name: str):
+        """_summary_
+
+        Args:
+            name (str): _description_
+        """
+        self.name = name
+        self.extent_row = 0
+        self.extent_col = 0
+        self.cells = {}  # {(row, col): Cell}
+
+    def add_cell(self, row: str, col: int, contents: str):
+        """_summary_
+
+        Args:
+            row (str): _description_
+            col (int): _description_
+            contents (str): _description_
+        """
+        # Call evalContents from Workbook Class
+        pass
+
+    def modify_cell(self, row: str, col: int, new_contents: str):
+        """_summary_
+
+        Args:
+            row (str): _description_
+            col (int): _description_
+            new_contents (str): _description_
+        """
+        pass
+
+    def delete_cell(self, row: str, col: int):
+        """_summary_
+
+        Args:
+            row (str): _description_
+            col (int): _description_
+        """
+        pass
+
+    def get_contents(self, row: str, col: int):
+        """_summary_
+
+        Args:
+            row (str): _description_
+            col (int): _description_
+        """
+        return self.cells[(row, col)].contents
