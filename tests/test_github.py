bb865208fbd7539e7925fd9c636927021ef41c6c
Jake Goldman <jgoldman@caltech.edu>
2023-01-11 20:28:27 -0800

    make test target; import sheets in test

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,9 +1,13 @@
+"""
+Simple test for Github push. 
+"""
+
 import unittest
 
-
 class GitAction(unittest.TestCase):
-    def test_foo(self):
+    def test_push(self):
         self.assertEqual("1", "1")
+        print("GitActions functional.")
 
 
 if __name__ == '__main__':
