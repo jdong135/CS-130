9a151b35864618c0bd1a6e95e974aa913ba388b4
Jake Goldman <jgoldman@caltech.edu>
2023-01-13 11:09:50 -0800

    remove parsing field from FormulaEvaluator

--- sheets/__init__.py
+++ sheets/__init__.py
@@ -2,6 +2,7 @@
 Spreadsheets Package developed for CS 130
 """
 from sheets.cell import *
+from sheets.cell_error import *
 from sheets.sheet import *
 from sheets.topo_sort import *
 from sheets.workbook import *
--- tests/test_lark_module.py
+++ tests/test_lark_module.py
@@ -5,6 +5,7 @@ Test for implementation of Lark Module formula Evaluator
 import unittest
 import os
 import sys
+import lark
 
 PROJECT_ROOT = os.path.abspath(os.path.join(
     os.path.dirname(__file__),
@@ -17,37 +18,43 @@ from sheets import *  # noqa
 class Lark_Module_Basic(unittest.TestCase):
     def test_single_dec(self):
         eval = FormulaEvaluator()
-        tree = eval.parser.parse("=1")
+        parser = lark.Lark.open('sheets/formulas.lark', start='formula')
+        tree = parser.parse("=1")
         value = eval.visit(tree)
         self.assertEqual(value, 1)
 
     def test_single_str(self):
         eval = FormulaEvaluator()
-        tree = eval.parser.parse("=\"str\"")
+        parser = lark.Lark.open('sheets/formulas.lark', start='formula')
+        tree = parser.parse("=\"str\"")
         value = eval.visit(tree)
         self.assertEqual(value, "str")
 
     def test_basic1(self):
         eval = FormulaEvaluator()
-        tree = eval.parser.parse("=1 + 7 - 124 * 7 / 14")
+        parser = lark.Lark.open('sheets/formulas.lark', start='formula')
+        tree = parser.parse("=1 + 7 - 124 * 7 / 14")
         value = eval.visit(tree)
         self.assertEqual(value, -54)
 
     def test_basic2(self):
         eval = FormulaEvaluator()
-        tree = eval.parser.parse("=2 * 6 - 4 + 2 / 2")
+        parser = lark.Lark.open('sheets/formulas.lark', start='formula')
+        tree = parser.parse("=2 * 6 - 4 + 2 / 2")
         value = eval.visit(tree)
         self.assertEqual(value, 9)
 
     def test_basic_parens(self):
         eval = FormulaEvaluator()
-        tree = eval.parser.parse("= 4 - 7 + (4 * 8 - 7) - 2 / 2 + (7 - 4)")
+        parser = lark.Lark.open('sheets/formulas.lark', start='formula')
+        tree = parser.parse("= 4 - 7 + (4 * 8 - 7) - 2 / 2 + (7 - 4)")
         value = eval.visit(tree)
         self.assertEqual(value, 24)
 
     def test_nested_parens(self):
         eval = FormulaEvaluator()
-        tree = eval.parser.parse("= 4 / 2 * (7 + 8 - (6 + (2 * 4)) - 4) + 7")
+        parser = lark.Lark.open('sheets/formulas.lark', start='formula')
+        tree = parser.parse("= 4 / 2 * (7 + 8 - (6 + (2 * 4)) - 4) + 7")
         value = eval.visit(tree)
         self.assertEqual(value, 1)
 
