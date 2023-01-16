92f85fa879dd2ea23243986e7bd2dbec7aafe037
Jake Goldman <jgoldman@caltech.edu>
2023-01-15 21:04:59 -0800

    update workbook tests; sheets docstrings

--- sheets/sheet.py
+++ sheets/sheet.py
@@ -1,16 +1,28 @@
+from typing import *
+
 class Sheet:
     def __init__(self, name: str):
-        """_summary_
+        """
+        Initialize an empty sheet object with a given name. This sheet has a current extent of (0, 0) and empty dictionary of cell locations to Cell objects. 
 
         Args:
-            name (str): _description_
+            name (str): name of the spreadsheet object.
         """
         self.name = name
         self.extent_row = 0
         self.extent_col = 0
         self.cells = {}  # {string: Cell}
 
-    def str_to_tuple(self, location: str):
+    def str_to_tuple(self, location: str) -> Tuple[int, int]:
+        """
+        Take in a string location ranging from A1 to ZZZZ9999 and return the integer coordinates of the location.
+
+        Args:
+            location (str): string location on the sheet.
+
+        Returns:
+            Tuple[int, int]: numeric coordinates on the sheet equivalent to input location. 
+        """
         chars = list(location)
         rows = 0
         cols = 0
@@ -25,7 +37,16 @@ class Sheet:
                 colCnt += 1
         return (cols, rows)
 
-    def check_valid_location(self, location: str):
+    def check_valid_location(self, location: str) -> bool:
+        """
+        Determine if a cell location is witin the range of ZZZZ9999.
+
+        Args:
+            location (str): string location on the sheet.
+
+        Returns:
+            bool: if the input location is in-bounds.
+        """
         col, row = self.str_to_tuple(location)
         if col > 475254 or row > 9999:
             return False
