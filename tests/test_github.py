0f5ceb5e3328aac1e56f204116c81bfcd06b4add
Jake Goldman <jgoldman@caltech.edu>
2023-01-15 19:25:38 -0800

    fix unittest relative paths

--- tests/test_github.py
+++ tests/test_github.py
@@ -8,8 +8,6 @@ import unittest
 class GitAction(unittest.TestCase):
     def test_push(self):
         self.assertEqual("1", "1")
-        print("GitActions functional.")
-
 
 if __name__ == '__main__':
     unittest.main()
