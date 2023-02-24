64da866968d0348c5a28edb6f3ffbcb65dcb1859
Jake Goldman <jgoldman@caltech.edu>
2023-02-23 22:47:44 -0800

    Implement all boolean functions

--- sheets/functions.py
+++ sheets/functions.py
@@ -57,11 +57,18 @@ class FunctionDirectory:
     def __init__(self):
         # Function name -> callable function
         self.directory: Dict[str, Callable[[List[Any]], Any]] = {
-            "AND": self.and_func
+            "AND": self.and_func,
+            "OR": self.or_func,
+            "NOT": self.not_func,
+            "XOR": self.xor_func
         }
 
     def call_function(self, func_name: str, args: List):
-        return self.directory[func_name.strip()](args)
+        try:
+            return self.directory[func_name.strip().upper()](args)
+        except KeyError:
+            return cell_error.CellError(
+                cell_error.CellErrorType.BAD_NAME, "Invalid Function name")
 
     def and_func(self, args: List):
         for arg in args:
@@ -84,3 +91,79 @@ class FunctionDirectory:
                     return cell_error.CellError(
                         cell_error.CellErrorType.TYPE_ERROR, "Invalid string argument")
         return True
+
+    def or_func(self, args: List):
+        for arg in args:
+            if isinstance(arg, Function):
+                arg = self.call_function(arg.name, arg.args)
+            if isinstance(arg, unitialized_value.UninitializedValue):
+                continue
+            if isinstance(arg, bool) and not arg:
+                continue
+            elif isinstance(arg, Decimal):
+                if arg == Decimal(0):
+                    continue
+            elif isinstance(arg, str) and string_conversions.is_number(arg):
+                if Decimal(arg) == Decimal(0):
+                    continue
+            elif isinstance(arg, str):
+                if arg.lower() == "false":
+                    continue
+                if arg.lower() != "true":
+                    return cell_error.CellError(
+                        cell_error.CellErrorType.TYPE_ERROR, "Invalid string argument")
+            return True
+        return False
+    
+    def not_func(self, args: List):
+        if len(args) != 1:
+            return cell_error.CellError(
+                        cell_error.CellErrorType.TYPE_ERROR, "Invalid argument count")
+        arg = args[0]
+        if isinstance(arg, Function):
+            arg = self.call_function(arg.name, arg.args)
+        if isinstance(arg, unitialized_value.UninitializedValue):
+            return True
+        if isinstance(arg, bool) and not arg:
+            return True
+        elif isinstance(arg, Decimal):
+            if arg == Decimal(0):
+                return True
+        elif isinstance(arg, str) and string_conversions.is_number(arg):
+            if Decimal(arg) == Decimal(0):
+                return True
+        elif isinstance(arg, str):
+            if arg.lower() == "false":
+                return True
+            if arg.lower() != "true":
+                return cell_error.CellError(
+                    cell_error.CellErrorType.TYPE_ERROR, "Invalid string argument")
+        return False
+
+    def xor_func(self, args: List):
+        if len(args) == 0:
+            return cell_error.CellError(
+                        cell_error.CellErrorType.TYPE_ERROR, "Invalid argument count")            
+        true_cnt = 0
+        for arg in args:
+            if isinstance(arg, Function):
+                arg = self.call_function(arg.name, arg.args)
+            if isinstance(arg, unitialized_value.UninitializedValue):
+                continue
+            if isinstance(arg, bool) and not arg:
+                continue
+            elif isinstance(arg, Decimal):
+                if arg == Decimal(0):
+                    continue
+            elif isinstance(arg, str) and string_conversions.is_number(arg):
+                if Decimal(arg) == Decimal(0):
+                    continue
+            elif isinstance(arg, str):
+                if arg.lower() == "false":
+                    continue
+                if arg.lower() != "true":
+                    return cell_error.CellError(
+                        cell_error.CellErrorType.TYPE_ERROR, "Invalid string argument")
+            true_cnt += 1
+        return true_cnt % 2 != 0
+    
